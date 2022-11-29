c3006f260284368dfa1a07e43fdd40ac75f62bba
Rohan Shah <rshah04@gmail.com>
2022-11-28 20:07:33 -0500

    added methods and documentation

---
+++ suduko.py
@@ -0,0 +1,14 @@
+'''When the program starts, it should display a Game Start screen. This will have buttons for the user to choose a 
+difficulty between easy, medium, or hard. '''
+
+import pygame
+
+'''Create a Sudoku Board 
+Difficulty  Number of empty cells  
+easy   30  
+medium  40  
+hard   50 '''
+
+'''Make "Game Won!" Screen with exit button if all cells are completed correctly"'''
+
+'''Make "Game Over :(" Screen with Restart button if the board is full but not solved correctly'''

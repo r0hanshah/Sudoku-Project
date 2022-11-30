17af406e71c894aea69155576b19badb7b52b21b
Rohan Shah <rshah04@gmail.com>
2022-11-30 15:40:13 -0500

    easy medium and hard buttons added

--- sudoku.py
+++ sudoku.py
@@ -2,29 +2,95 @@
 # Reference "HOW TO MAKE A MENU SCREEN IN PYGAME!" https://www.youtube.com/watch?v=GMBqjxcKogA  
 # Reference "Pygame Tutorial: How to Make a Button" https://www.youtube.com/watch?v=J5bIPtEbS0A
 import pygame
+import sys
 
 '''When the program starts, it should display a Game Start screen. This will have buttons for the user to choose a 
 difficulty between easy, medium, or hard. '''
 
 # Setting the dimensions of the window
-color = (255,255,255) 
 pygame.init()
-height = 850
-width = 600
-display = pygame.display.set_mode((width,height))
+HEIGHT = 800
+WIDTH = 600
+display = pygame.display.set_mode((WIDTH,HEIGHT))
+
+def start_menu(display):
+    start_title_font = pygame.font.Font("OptimusPrinceps.ttf", 100)
+    button_font = pygame.font.Font("OptimusPrinceps.ttf", 50)
+
+    # Color background
+    display.fill("black")
+
+    # Initialize and draw title
+    title_surface = start_title_font.render("Sudoku", 0, "white")
+    title_rectangle = title_surface.get_rect(
+        center=(WIDTH // 2, HEIGHT // 2 - 200))
+    display.blit(title_surface, title_rectangle)
+
+    # Initialize buttons
+    # Initialize text first
+    easy_text = button_font.render("Easy", 0, "white")
+    medium_text = button_font.render("Medium", 0, "white")
+    hard_text = button_font.render("Hard", 0, "white")
+    quit_text = button_font.render("Quit", 0, "white")
+
+    # Initialize button background color and text
+    start_surface = pygame.Surface((easy_text.get_size()[0] + 20, easy_text.get_size()[1] + 20))
+    start_surface.fill("black")
+    start_surface.blit(easy_text, (10, 10))
+    medium_surface = pygame.Surface((medium_text.get_size()[0]+ 20, medium_text.get_size()[1]+20))
+    medium_surface.fill("black")
+    medium_surface.blit(medium_text, (10, 10))
+    hard_surface = pygame.Surface((hard_text.get_size()[0]+ 20, hard_text.get_size()[1]+20))
+    hard_surface.fill("black")
+    hard_surface.blit(hard_text, (10, 10))
+    quit_surface = pygame.Surface((quit_text.get_size()[0] + 20, quit_text.get_size()[1] + 20))
+    quit_surface.fill("black")
+    quit_surface.blit(quit_text, (10, 10))
+
+    # Initialize button rectangle
+    start_rectangle = start_surface.get_rect(
+        center=(WIDTH // 2, HEIGHT // 2 -50))
+
+    medium_rectangle = medium_surface.get_rect(
+        center = (WIDTH//2, HEIGHT//2 + 50)
+    )
+    hard_rectangle = hard_surface.get_rect(
+        center = (WIDTH//2, HEIGHT//2 + 150)
+    )
+
+    quit_rectangle = quit_surface.get_rect(
+        center=(WIDTH // 2, HEIGHT // 2 + 250))
+
+    # Draw buttons
+    display.blit(start_surface, start_rectangle)
+    display.blit(medium_surface, medium_rectangle)
+    display.blit(hard_surface, hard_rectangle)
+    display.blit(quit_surface, quit_rectangle)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if start_rectangle.collidepoint(event.pos):
+                    # Checks if mouse is on start button
+                    return  # If the mouse is on the start button, we can return to main
+                elif quit_rectangle.collidepoint(event.pos):
+                    # If the mouse is on the quit button, exit the program
+                    sys.exit()
+        pygame.display.update()
 
 # Create caption with font
 pygame.display.set_caption("Sudoku")
 font = pygame.font.Font('OptimusPrinceps.ttf',55) 
 
 
-suduko_text = font.render(f'Sudoku:', True, 'white')
-# Create background
-background = pygame.image.load("menubackground.jpeg")
+#suduko_text = font.render(f'Sudoku:', True, 'white')
+
 # Updates window with new background
 pygame.display.update() 
 #Menu Title "sudoku"
-display.blit(suduko_text, (200, 150))
+#display.blit(suduko_text, (200, 150))
 
 class Button:
     def __init__(self, x_cord, y_cord, image, text_input):
@@ -73,13 +139,12 @@ class Button:
 
         display.blit(self.text, self.text_rect) #^ this might do the same thing
         
-
 # Run the program
 def main():
     running = True
     
     #creating easy button
-    button = Button(100, 100, None, "Easy")
+    #button = Button(100, 100, None, "Easy")
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -96,12 +161,12 @@ def main():
         #display.blit(suduko_text, (200, 150))
 
         
-        if button.draw_text(display):
+        #if button.draw_text(display):
             #INSERT WHAT PRESSING EASY BUTTON DOES
-            display.fill("black")
-            pygame.display.update()
-
-        pygame.display.update()
+            #display.fill("black")
+            #pygame.display.update()
+        start_menu(display)
+        #pygame.display.update()
         
         
         
